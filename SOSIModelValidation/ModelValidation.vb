d2058f72130108df6266ff25a24a0b8c54614dc2
aatjora <asmund.tjora@kartverket.no>
2018-10-18 14:32:47 +0200

    Revert "reqUMLIntegration halvveis"
    This reverts commit b5c4589fc7ef946988469c690dca318890f79df1.

--- SOSIModelValidation/ModelValidation.vb
+++ SOSIModelValidation/ModelValidation.vb
@@ -3,7 +3,6 @@
     Dim versionYear = "2018"
     Dim errorCounter As Integer
     Dim warningCounter As Integer
-    Dim omittedCounter As Integer
     Dim logLevel = "Warning"
     Dim ruleSet = "SOSI"
     Dim startTime, endTime, elapsedTime
@@ -45,16 +44,20 @@
             Case EA.ObjectType.otPackage
                 thePackage = theRepository.GetTreeSelectedObject()
                 If Not thePackage.IsModel Then
-                    Dim messageText = "SOSI Model Validation add-in" + vbCrLf + "version " + versionNumber + vbCrLf + "Kartverket " + versionYear + vbCrLf + vbCrLf
-                    messageText = messageText + "Model validation based on requirements and recommendations in SOSI standard 'Regler for UML-modellering 5.0'" + vbCrLf + vbCrLf
-                    messageText = messageText + "Selected package: «" + thePackage.Element.Stereotype + "» " + thePackage.Element.Name
-                    validationWindow.Label1.Text() = messageText
-                    validationWindow.Show()
+                    If UCase(thePackage.Element.Stereotype) = UCase("applicationSchema") Then
+                        Dim messageText = "SOSI Model Validation add-in" + vbCrLf + "version " + versionNumber + vbCrLf + "Kartverket " + versionYear + vbCrLf + vbCrLf
+                        messageText = messageText + "Model validation based on requirements and recommendations in SOSI standard 'Regler for UML-modellering 5.0'" + vbCrLf + vbCrLf
+                        messageText = messageText + "Selected package: «" + thePackage.Element.Stereotype + "» " + thePackage.Element.Name
+                        validationWindow.Label1.Text() = messageText
+                        validationWindow.Show()
+                    Else
+                        System.Windows.Forms.MessageBox.Show("Please select a package with stereotype «applicationSchema».")
+                    End If
                 Else
-                    System.Windows.Forms.MessageBox.Show("Please select a package in the project browser.")
+                    System.Windows.Forms.MessageBox.Show("Please select a package with stereotype «applicationSchema».")
                 End If
             Case Else
-                System.Windows.Forms.MessageBox.Show("Please select a package in the project browser.")
+                System.Windows.Forms.MessageBox.Show("Please select a package with stereotype «applicationSchema».")
         End Select
     End Sub
 
@@ -92,9 +95,6 @@
         If logLevel = "Warning" Then
             Output("Number of warnings found: " & warningCounter)
         End If
-        If omittedCounter > 0 Then
-            Output("Number of omitted tests:" & omittedCounter)
-        End If
         Output("Time used: " & FormatNumber(elapsedTime, 2))
         Output("-----------------------------------")
     End Sub
@@ -104,7 +104,6 @@
         'initialize variables
         errorCounter = 0
         warningCounter = 0
-        omittedCounter = 0
         startTime = Timer
         packageIDList.Clear()
         classifierIDList.Clear()
@@ -155,8 +154,6 @@
             'check if there are packages with stereotype "applicationSchema"in package hierarchy upwards from start package
             'CheckParentPackageStereotype(thePackage)
             Call reqUMLProfileLoad()
-
-            Call reqUMLIntegration(thePackage)
 
             ' Tests that should be done recursivly on subpackages should called in FindInvalidElementsInPackage
             Call FindInvalidElementsInPackage(thePackage)
