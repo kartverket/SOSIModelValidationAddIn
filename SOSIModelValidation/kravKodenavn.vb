9a181a4846048e5bd740c3b9670dc17f976e9fca
Kent Jonsrud <kent.jacob.jonsrud@gmail.com>
2020-08-18 12:24:45 +0200

    forbedret meldingstekstene

--- SOSIModelValidation/kravKodenavn.vb
+++ SOSIModelValidation/kravKodenavn.vb
@@ -2,46 +2,52 @@
 
     'Sub name:      kravKodenavn
     'Author: 		Kent Jonsrud
-    'Date: 			2020-08-17
+    'Date: 			2020-08-17/-18
     'Purpose: 		'/krav/Kodenavn is a national adaption of Iso 19103 Requirement 6 - legal NonWhitespaceNames on code list code names. If a initial value (Default) exists then check for valid and unique NWNAmes in all initial values for this codelist.
-    'Parameter: 	an element that has stereotype CodeList or Enumeration, or keyword enumeration (EA-type Enumeration)
+    'Parameter: 	an element that has stereotype CodeList or Enumeration, or has keyword enumeration (EA-type Enumeration)
     'Requirement class:     n/a
     'Conformance class:     Regler for UML-modellering versjon 5.1:2020 - Basisregler for UML med nasjonale tilpassinger (innstramminger/utelatelser)
 
 
     Sub kravKodenavn(theThing As EA.Element)
-        Call kravKodenavnonElement(theThing)
+        Call kravKodenavnOnElement(theThing)
     End Sub
-    Sub kravKodenavnonElement(theElement)
+    Sub kravKodenavnOnElement(theElement)
         Dim CodeNames As New System.Collections.ArrayList
         Dim CodeDefaults As New System.Collections.ArrayList
+        Dim kw = theElement.Stereotype
+        If kw = "" Then
+            kw = "enumeration"
+        End If
 
         For Each attribute In theElement.Attributes
             If attribute.Default <> "" Then
-                ' test whether the default value is a unique NWName, ignore code name
+                ' test whether the initial value is a case insensitively unique NWName, ignoring the spelling of the code name
                 If Not isNWName(attribute.Default) Then
-                    Output("Error: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has illegal character in code initial value [" & attribute.Name & "] [" & attribute.Default & "]. [/krav/kodenavn]")
+                    Output("Error: Class [«" & kw & "» " & theElement.Name & "] code name [" & attribute.Name & "] has illegal character in code initial value [" & attribute.Default & "]. [/krav/kodenavn]")
                     errorCounter += 1
                 End If
                 If CodeDefaults.IndexOf(UCase(attribute.Default), 0) <> -1 Then
-                    Output("Error: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has non-unique code initial value [" & attribute.Name & "] [" & attribute.Default & "]. [/krav/16]")
+                    Output("Error: Class [«" & kw & "» " & theElement.Name & "] code name [" & attribute.Name & "] has non-unique code initial value [" & attribute.Default & "]. [/krav/kodebruk]")
                     errorCounter += 1
                 Else
-                    CodeNames.Add(UCase(attribute.Name))
+                    If CodeNames.IndexOf(UCase(attribute.Name), 0) = -1 Then
+                        CodeNames.Add(UCase(attribute.Name))
+                    End If
                     CodeDefaults.Add(UCase(attribute.Default))
                 End If
             Else
-                ' test whether the code name is a unique NWName (and no default is present)
+                ' test whether the code name is a case insensitively unique NWName when no initial value is present
                 If Not isNWName(attribute.Name) Then
-                    Output("Error: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has illegal character in code name [" & attribute.Name & "]. [/krav/6]")
+                    Output("Error: Class [«" & kw & "» " & theElement.Name & "] has illegal character in code name [" & attribute.Name & "]. [/krav/6]")
                     errorCounter += 1
                 End If
                 If Not isLCNameX(attribute.Name) Then
-                    Output("Warning: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has code name starting with uppercase character or letter [" & attribute.Name & "]. [/krav/6]")
+                    Output("Warning: Class [«" & kw & "» " & theElement.Name & "] has code name starting with uppercase character or letter [" & attribute.Name & "]. [/krav/6]")
                     warningCounter += 1
                 End If
                 If CodeNames.IndexOf(UCase(attribute.Name), 0) <> -1 Then
-                    Output("Error: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has non-unique code names [" & attribute.Name & "]. [/krav/16]")
+                    Output("Error: Class [«" & kw & "» " & theElement.Name & "] has non-unique code names [" & attribute.Name & "]. [/krav/16]")
                     errorCounter += 1
                 Else
                     CodeNames.Add(UCase(attribute.Name))
@@ -50,7 +56,7 @@
         Next
 
         If CodeDefaults.Count <> 0 And CodeNames.Count <> CodeDefaults.Count Then
-            Output("Error: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] has a mix of codes with, and without initial values [" & CodeNames.Count - CodeDefaults.Count & "]. [/krav/kodebruk]")
+            Output("Error: Class [«" & kw & "» " & theElement.Name & "] has a mix of codes with [" & CodeDefaults.Count & "] and without [" & CodeNames.Count - CodeDefaults.Count & "] valid initial values. [/krav/kodebruk]")
             errorCounter += 1
         End If
 
