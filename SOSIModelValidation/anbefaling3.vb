6e1704af6115364c56d53cabcdc01e0e19b703df
toreJohnsen <tore.johnsen@kartverket.no>
2021-07-01 09:18:54 +0200

    Revert "Fixed #141"
    This reverts commit ee9c5761dd7ddc326dfcec0e7d33bec69af38c39.

--- SOSIModelValidation/anbefaling3.vb
+++ SOSIModelValidation/anbefaling3.vb
@@ -23,33 +23,30 @@
             counterNotes = False
 
             For Each secondaryAtt In theElement.Attributes
-				
-				If UCase(mainAtt.Name) = UCase(secondaryAtt.Name) Then
-					If mainAtt.AttributeID <> secondaryAtt.AttributeID Then
-						duplicatedCodesString = duplicatedCodesString + " " + "[" & secondaryAtt.Name & "]"
-						counterName = True
-					End If
-				End If
-				
-				
-				If mainAtt.Notes <> ""
-					If Trim(UCase(mainAtt.Notes)) = Trim(UCase(secondaryAtt.Notes)) Then
-						If mainAtt.AttributeID <> secondaryAtt.AttributeID Then
-							duplicatedNotesString = duplicatedNotesString + " " + "[" & secondaryAtt.Name & "]"
-							counterNotes = True
-						End If
-					End If
-				End If
+
+                If UCase(mainAtt.Name) = UCase(secondaryAtt.Name) Then
+                    If mainAtt.AttributeID <> secondaryAtt.AttributeID Then
+                        duplicatedCodesString = duplicatedCodesString + " " + "[" & secondaryAtt.Name & "]"
+                        counterName = True
+                    End If
+                End If
+
+                If Trim(UCase(mainAtt.Notes)) = Trim(UCase(secondaryAtt.Notes)) Then
+                    If mainAtt.AttributeID <> secondaryAtt.AttributeID Then
+                        duplicatedNotesString = duplicatedNotesString + " " + "[" & secondaryAtt.Name & "]"
+                        counterNotes = True
+                    End If
+                End If
 
             Next
 
             If counterName Then
-                Output("Waning: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] - attribute: [" & mainAtt.Name & "] has the same codename as" & duplicatedCodesString & ". [/anbefaling/3]")
+                Output("Waning: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] - attribute: [" & mainAtt.Name & "] has the same codename as" & duplicatedCodesString & ". [/anbefaling/6]")
                 warningCounter += 1
             End If
 
             If counterNotes Then
-                Output("Waning: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] - attribute: [" & mainAtt.Name & "] has the same definition as" & duplicatedNotesString & ". [/anbefaling/3]")
+                Output("Waning: Class [«" & theElement.Stereotype & "» " & theElement.Name & "] - attribute: [" & mainAtt.Name & "] has the same definition as" & duplicatedNotesString & ". [/anbefaling/6]")
                 warningCounter += 1
             End If
 
